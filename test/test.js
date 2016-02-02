82fb56b8d5f3cc06cab29ff9a275015c2026be4b
Jamie Little <j.little@miami.edu>
2016-02-01 21:13:51 -0500

    Updating tests

--- test/test.js
+++ test/test.js
@@ -1,17 +1,38 @@
 var assert = require('assert'),
-    Handbag = require('./../index').Handbag;
+    Gladstone = require('./../index').Gladstone;
     fs = require('fs');
-describe('Handbag', function () {
-
+describe('Gladstone', function () {
     describe('#processArgs', function () {
         it('should return an object with the bag name and the origin directory', function () {
-           
-           var args = ['node', 'index.js', 'bag_name', 'origin_directory'];
-           var myArgs = Handbag.processArgs(args);
-        
-           assert.equal(myArgs.bagName, 'bag_name');
-           assert.equal(myArgs.originDirectory, 'origin_directory');
+
+            var args = ['node', 'index.js', 'bag_name', 'origin_directory'];
+            var myArgs = Gladstone.processArgs(args);
+
+            assert.equal(myArgs.bagName, 'bag_name');
+            assert.equal(myArgs.originDirectory, 'origin_directory');
 
         });
+
+        describe('#createBagDirectory', function () {
+            it('show create a directory for the bag', function () {
+
+            });
+        });
+
+        describe('#writeBagInfo', function () {
+            it('should write a text file for the bag', function () {
+
+            });
+
+        });
+
+        describe('#createManifest', function () {
+            it('should create a manifest file', function () {
+
+            });
+        });
+
+
+
     });
 });
