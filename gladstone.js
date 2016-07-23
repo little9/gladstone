726948b81820acacf1453463258e45e79990b245
Jamie Little <j.little@miami.edu>
2016-07-22 20:28:08 -0400

    functions returning true if successful

--- gladstone.js
+++ gladstone.js
@@ -13,118 +13,127 @@ var lastdirpath = require('./lib/lastdirpath');
 var processArgs = require('./lib/process-args');
 
 module.exports = {
-    createBagDirectory: function (args) {
-         var procArgs = processArgs(args);
-        return new Promise(function (resolve, reject) {
-            fs.mkdir(args.bagName, function (err) {
-                if (err) {
-                    console.error(strings.errorBagCreation);
-                }
+  createBagDirectory: function (args) {
+    var procArgs = processArgs(args);
+    return new Promise(function (resolve, reject) {
+                         fs.mkdir(args.bagName, function (err) {
+                           if (err) {
+                             console.error(strings.errorBagCreation);
+                           }
 
-                console.log(strings.createdBag + procArgs.bagName);
-                fs.mkdir(procArgs.bagName + '/data', function (err) {
-                    if (err) throw err;
-                    console.log(strings.createdData + '/data');
-                    module.exports.writeBagInfo(procArgs);
-                    module.exports.copyOriginToData(procArgs);
+                           console.log(strings.createdBag + procArgs.bagName);
+                           fs.mkdir(procArgs.bagName + '/data', function (err) {
+                             if (err) throw err;
+                             console.log(strings.createdData + '/data');
+                             module.exports.writeBagInfo(procArgs);
+                             module.exports.copyOriginToData(procArgs);
 
-                    setTimeout(function () {
-                        resolve(true);
-                    }, 200);
-                });
+                             setTimeout(function () {
+                               resolve(true);
+                             }, 200);
+                           });
+                         });
+                       });
 
-            });
+  },
+  writeBagInfo: function (args, bagInfoMetadata) {
+    fs.writeFile(args.bagName + '/' + 'bagit.txt', strings.bagIt + "\n", function (err) {
+      if (err) {
+        return console.error(strings.errorBagInfo);
+      }
+      return console.log(strings.createdBagInfo + args.bagName + '/' + 'bagit.txt');
+    });
+    
+    fs.writeFile(args.bagName + '/' + 'bag-info.txt',"", function (err) {
+      if (err) {
+        return console.error(strings.errorBagInfo);
+      }
+      return console.log(strings.createdBagInfo + args.bagName + '/' + 'bag-info.txt');
+    });
+
+    for (var i in bagInfo) {
+      if (bagInfo[i]) {
+        fs.appendFile(args.bagName + '/' + 'bag-info.txt', i + ": " + bagInfo[i] + "\n", function (err) {
+          if (err) {
+            return console.error(strings.errorBagInfo);
+          }
+           return true;
         });
-
-    },
-    writeBagInfo: function (args, bagInfoMetadata) {
-        fs.writeFile(args.bagName + '/' + 'bag-info.txt', strings.bagInfoTxt + "\n", function (err) {
-            if (err) {
-                return console.error(strings.errorBagInfo);
-            }
-            console.log(strings.createdBagInfo + args.bagName + '/' + 'bag-info.txt');
-        });
-
-        for (var i in bagInfo) {
-            if (bagInfo[i]) {
-                fs.appendFile(args.bagName + '/' + 'bag-info.txt', i + ": " + bagInfo[i] + "\n", function (err) {
-                    if (err) {
-                        return console.error(strings.errorBagInfo);
-                    }
-                });
-            }
-        }
-
-    },
-    copyOriginToData: function (args) {
-        var lastDirPath = lastdirpath.getLastDirPath(args.originDirectory);
-        fs.mkdir(args.bagName + '/data/' + lastDirPath, function (err) {
-            if (err) throw err;
-        });
-
-        ncp(args.originDirectory, args.bagName + '/data/' + lastDirPath, function (err) {
-            if (err) {
-                return console.error(strings.errorCopying);
-            }
-            module.exports.createManifest(args.bagName + '/data', args, 'manifest');
-        });
-    },
-    getRelativePath: function (filePath) {
-        var relPath = filePath.replace(/\\\\/g, '').replace(/\//g, '/');
-        var splitName = relPath.split('/data/');
-        var relName = relPath.replace(splitName[0], '');
-
-        if (relName.substring(0, 1) == '/') {
-            relName = relName.substring(1);
-        }
-
-        return relName;
-    },
-    /**
-     * Functions for creating the manifest file.
-     */
-    createManifest: function (myPath, args, type) {
-        // 1 Recurse through the path provided and run the createFileHash function on all the files
-        var manifestFileName = module.exports.getManifestFileName(args.bagName, args.cryptoMethod, type);
-        console.log(strings.writingManifest + manifestFileName);
-        if (type === 'manifest') {
-            recursive(myPath, function (err, files) {
-                files.forEach(function (file) {
-                    module.exports.createFileHash(file, args, manifestFileName);
-                });
-            });
-        }
-
-    },
-    createFileHash: function (file, args, manifestFileName) {
-        // 2 Create a hash for the provided file path and send the results to the appendHashtoManifest function
-        var hash = crypto.createHash(args.cryptoMethod);
-        var stats = fs.stat(file, function (err, stat) {
-            if (!stat.isDirectory()) {
-                var stream = fs.createReadStream(file);
-                stream.on('data', function (data) {
-                    hash.update(data, 'utf8');
-                });
-                stream.on('end', function () {
-                    var myHash = hash.digest('hex');
-                    module.exports.appendHashtoManifest(myHash, file, manifestFileName, args)
-                });
-            }
-        });
-    },
-    appendHashtoManifest: function (hash, file, manifestFileName, args) {
-        // 3 Append the combined hash and filename to the manifest file returned by the getManifestFileName function
-        var relName = module.exports.getRelativePath(file);
-        var manifestLine = hash + ' ' + relName + '\n';
-        fs.appendFile(manifestFileName, manifestLine, function (err) {
-            if (err) {
-                return console.error(strings.errorManifest);
-            }
-        });
-    },
-    getManifestFileName: function (bagName, cryptoMethod, type) {
-        // 4 Get the full path of the manifest file
-        var manifestFileName = bagName + '/' + type + '-' + cryptoMethod + '.txt';
-        return manifestFileName;
+      }
     }
+
+  },
+  copyOriginToData: function (args) {
+    var lastDirPath = lastdirpath.getLastDirPath(args.originDirectory);
+    fs.mkdir(args.bagName + '/data/' + lastDirPath, function (err) {
+      if (err) throw err;
+    });
+
+    ncp(args.originDirectory, args.bagName + '/data/' + lastDirPath, function (err) {
+      if (err) {
+        return console.error(strings.errorCopying);
+      }
+      module.exports.createManifest(args.bagName + '/data', args, 'manifest');
+      return true;
+    });
+  },
+  getRelativePath: function (filePath) {
+    var relPath = filePath.replace(/\\\\/g, '').replace(/\//g, '/');
+    var splitName = relPath.split('/data/');
+    var relName = relPath.replace(splitName[0], '');
+
+    if (relName.substring(0, 1) == '/') {
+      relName = relName.substring(1);
+    }
+
+    return relName;
+  },
+  /**
+   * Functions for creating the manifest file.
+   */
+  createManifest: function (myPath, args, type) {
+    // 1 Recurse through the path provided and run the createFileHash function on all the files
+    var manifestFileName = module.exports.getManifestFileName(args.bagName, args.cryptoMethod, type);
+    console.log(strings.writingManifest + manifestFileName);
+    if (type === 'manifest') {
+      recursive(myPath, function (err, files) {
+        files.forEach(function (file) {
+          module.exports.createFileHash(file, args, manifestFileName);
+        });
+      });
+    }
+
+  },
+  createFileHash: function (file, args, manifestFileName) {
+    // 2 Create a hash for the provided file path and send the results to the appendHashtoManifest function
+    var hash = crypto.createHash(args.cryptoMethod);
+    var stats = fs.stat(file, function (err, stat) {
+                  if (!stat.isDirectory()) {
+                    var stream = fs.createReadStream(file);
+                    stream.on('data', function (data) {
+                      hash.update(data, 'utf8');
+                    });
+                    stream.on('end', function () {
+                      var myHash = hash.digest('hex');
+                      module.exports.appendHashtoManifest(myHash, file, manifestFileName, args)
+                    });
+                  }
+                });
+  },
+  appendHashtoManifest: function (hash, file, manifestFileName, args) {
+    // 3 Append the combined hash and filename to the manifest file returned by the getManifestFileName function
+    var relName = module.exports.getRelativePath(file);
+    var manifestLine = hash + ' ' + relName + '\n';
+    fs.appendFile(manifestFileName, manifestLine, function (err) {
+      if (err) {
+        return console.error(strings.errorManifest);
+      }
+      return true;
+    });
+  },
+  getManifestFileName: function (bagName, cryptoMethod, type) {
+    // 4 Get the full path of the manifest file
+    var manifestFileName = bagName + '/' + type + '-' + cryptoMethod + '.txt';
+    return manifestFileName;
+  }
 };
